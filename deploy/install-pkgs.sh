8d17f744fd7fcf23cb0ac9fc1063859410ddd693
Scott Smereka <scott.smereka@gmail.com>
2016-06-15 17:13:45 -0400

    Trying another deploy script

--- deploy/install-pkgs.sh
+++ deploy/install-pkgs.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 cd /var/www/sdlserver
-apt-get install git
 npm install --prefix /var/www/sdlserver
-./node_modules/bower/bin/bower install --allow-root
+#./node_modules/bower/bin/bower install --allow-root
