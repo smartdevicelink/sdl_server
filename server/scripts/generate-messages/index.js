d2281b4097c31186bf474d56ffc48be055f2ad7d
Christopher Rokita <chris@livio.io>
2017-10-11 15:07:56 -0400

    Move all data to migration scripts

--- server/scripts/generate-messages/index.js
+++ server/scripts/generate-messages/index.js
@@ -1,57 +1,49 @@
-var path = require('path'),
-  policy = require(path.resolve('policy.json')),
-  sqlBricks = require('sql-bricks'); // generates SQL statements to be logged to the console
+//Generates the SQL commands necessary to insert this data into the database. For dev use only.
+const path = require('path');
+const utils = require('../utils.js');
+const policy = require(path.resolve('../policy.json'));
+const sql = require('sql-bricks'); //generates SQL statements to be logged to the console
+const messages = policy.policy_table.consumer_friendly_messages.messages; //consumer friendly messages object
+let outputBuffer = ""; //what will get written to file
+const outputFileName = 'output.log'; //the name of the output file
 
-var messages = policy.policy_table.consumer_friendly_messages.messages;
+//LANGUAGE DEFINITION OUTPUT
+//find the first message category and generate the languages table from its languages
+const languages = messages[Object.keys(messages)[0]].languages;
+const languageNameObjs = Object.keys(languages).map(function (languageName) {
+	return {
+		id: languageName
+	};
+});
+const languageStatement = sql.insert('languages').values(languageNameObjs).toString();
+writeToBuffer(utils.formatInsertOutput(languageStatement));
 
-var insertInto = sqlBricks.insertInto;
-
-/*
-console.log('CREATE TABLE languages (' +
-            '"name" CHAR(5) NOT NULL,' +
-            'PRIMARY KEY ("name")' +
-            ');') // create languages table with necessary columns
-*/
-console.log('INSERT INTO "languages" ("name")\nVALUES')
-
- // find the first message category and generate languages table from its languages
-let languages = messages[Object.keys(messages)[0]].languages,
-    sql = ''
-for(var name in languages){
-  // generate a single INSERT INTO statement by adding each as a value on a new line
-  sql += insertInto('languages', 'name').values(name)
-        .toString()
-        .replace('INSERT INTO languages (name) VALUES ', '') + ',\n'
+//CONSUMER FRIENDLY MESSAGE OUTPUT
+//transform the language text into SQL insertion statements
+let messageObjs = [];
+for (let messageCategory in messages) {
+	const catObj = messages[messageCategory];
+	const languageObjs = Object.keys(catObj.languages).map(function (languageName) {
+		const langObj = catObj.languages[languageName];
+		return {
+			label: langObj.label,
+			language_id: languageName,
+			line1: langObj.line1,
+			line2: langObj.line2,
+			message_category: messageCategory,
+			text_body: langObj.text_body,
+			tts: langObj.tts
+		};
+	});
+	messageObjs = messageObjs.concat(languageObjs);
 }
-console.log(sql.slice(0, sql.length - 2) + ';') // drop the hanging comma and \n for an end of statement
-/*
-console.log('CREATE TABLE "message_text" ( ' +
-            '"language_id" TEXT REFERENCES languages (name) ON UPDATE CASCADE ON DELETE CASCADE,' + // languages are verified and referenced from the languages table
-            '"message_category" TEXT NOT NULL,' +
-            '"tts" TEXT,' +
-            '"line1" TEXT,' +
-            '"line2" TEXT,' +
-            '"text_body" TEXT,' +
-            '"status" TEXT NOT NULL DEFAULT \'STAGING\',' +
-            '"label" TEXT,' +
-            '"created_ts" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),' +
-            '"updated_ts" TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),' +
-            'PRIMARY KEY (language_id, message_category)' +
-            ');') // create message_text table with necessary columns
-*/
-console.log('INSERT INTO "message_text"("label", "language_id", "line1", "line2", "message_category","text_body", "tts")')
-console.log('VALUES')
-sql = ''
-for(var message_category in messages){
-  let languages = messages[message_category].languages
-  // generate a single INSERT INTO statement for each as a value on a new line
-  for(var id in languages){
-    let language_id = languages[id]
-    sql += insertInto('message_text', 'label', 'language_id', 'line1', 'line2', 'message_category', 'text_body', 'tts')
-          .values(language_id.label, id, language_id.line1, language_id.line2, message_category, language_id.textBody, language_id.tts)
-          .toString()
-          .replace('INSERT INTO message_text (label, language_id, line1, line2, message_category, text_body, tts) VALUES ', '') + ',\n'
-  }
+
+const messageStatement = sql.insert('message_text').values(messageObjs).toString();
+writeToBuffer(utils.formatInsertOutput(messageStatement));
+
+//write to file and finish
+utils.writeToFile(outputFileName, outputBuffer);
+
+function writeToBuffer (string) {
+    outputBuffer += string;   
 }
-console.log(sql.slice(0, sql.length - 2) + ';') // drop the hanging comma and \n for an end of statement
-console.log('SELECT * FROM message_text ORDER BY message_category, language_id') //display the table and order by category first then id
