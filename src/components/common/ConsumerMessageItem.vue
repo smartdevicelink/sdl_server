f43f7f92b80288cfde40f5c3708e41921109d12a
Nick Schwab <nick.schwab@livio.io>
2018-01-23 10:43:12 -0500

    - fade effect for is_deleted consumer messages

--- src/components/common/ConsumerMessageItem.vue
+++ src/components/common/ConsumerMessageItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <router-link v-bind:to="{ path: 'consumermessages/manage', query: { message_category: item.message_category, intent: 'edit'}}">
+    <router-link v-bind:to="{ path: 'consumermessages/manage', query: { message_category: item.message_category, intent: 'edit'}}" v-bind:class="{ 'opacity-30': item.is_deleted }">
         <div>
             <h5>{{ item.message_category }}<i class="pull-right fa fa-pencil display-hover" aria-hidden="true"></i></h5>
             <div class="description">
