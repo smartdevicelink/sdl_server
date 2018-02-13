19eea8c2a72d3d6671139cc709e7005d072226eb
Nick Schwab <nick.schwab@livio.io>
2018-02-13 14:58:57 -0500

    Prep Postgres handler to support SQL transactions

--- custom/databases/postgres/index.js
+++ custom/databases/postgres/index.js
@@ -1,6 +1,21 @@
 //PostgreSQL Communication Module
 const pg = require('pg'); //handles connections to the postgres database
+const ASYNC = require('async');
 //get configurations from environment variables
+
+// extend the Postgres client to easily fetch a single expected result as an object
+pg.Client.prototype.getOne = pg.Pool.prototype.getOne = function(query, values, callback){
+    this.query(query, values, function(err, result) {
+        callback(err, result && Array.isArray(result.rows) && result.rows.length ? result.rows[0] : null);
+    });
+};
+
+// extend the Postgres client to easily fetch multiple expected results as an array
+pg.Client.prototype.getMany = pg.Pool.prototype.getMany = function(query, values, callback){
+    this.query(query, values, function(err, result){
+        callback(err, (result && result.rows) ? result.rows : []);
+    });
+};
 
 let config = {
     max: 10,
@@ -31,7 +46,7 @@ module.exports = function (log) {
     //listen for errors
     pool.on('error', function (err, client) {
         log.error('idle client error', err.message, err.stack);
-    });  
+    });
 
     const self = {
         //exported functions. these are required to implement
@@ -39,7 +54,7 @@ module.exports = function (log) {
         //the callback requires an error parameter and a response from the SQL query
         sqlCommand: function (query, callback) {
             pool.query(query, function (err, res) {
-                //always return an array 
+                //always return an array
                 callback(err, (res && res.rows) ? res.rows : []);
             });
         },
@@ -51,7 +66,7 @@ module.exports = function (log) {
                     log.error(sqlString);
                 }
                 next(err, res);
-            });            
+            });
         },
         setupSqlCommands: function (sqlStringArray, propagateErrors) {
             if (!Array.isArray(sqlStringArray)) { //if its just a single sql statement, make it into an array
@@ -70,10 +85,64 @@ module.exports = function (log) {
                         else {
                             next(null, res); //do not propagate errors. if an error happens, continue anyway
                         }
-                        
+
                     });
                 }
-            });            
+            });
+        },
+        getClient: function (callback){
+            // reserve a client connection ("client") and its associated release callback ("done")
+            // callback(err, client, done)
+            pool.connect(callback);
+        },
+        rollback: function (client, done){
+            // rolls back and releases a client from the pool
+            client.query("ROLLBACK", function(err){
+                done(err);
+            });
+        },
+        commit: function (client, done, callback){
+            // commits and releases a client from the pool
+            client.query("COMMIT", function(err, result){
+                done(err);
+                callback(err, result);
+            });
+        },
+        runQueriesAsTransaction: function (queries, callback){
+            var dbClient = null,
+                dbDone = null;
+            ASYNC.waterfall([
+                function(callback){
+                    // get a new client
+                    self.getClient(callback);
+                },
+                function(client, done, callback){
+                    dbClient = client;
+                    dbDone = done;
+                    // start a transaction
+                    client.query("BEGIN", function(err){
+                        if(err) self.rollback(client, done);
+                        callback(err, client, done);
+                    });
+                },
+                function(client, done, callback){
+                    // run queries in order provided
+                    ASYNC.eachSeries(queries, function(query, callback){
+                        client.query(query, function(err, result){
+                            if(err) self.rollback(client, done);
+                            callback(err);
+                        });
+                    }, function(err){
+                        callback(err, client, done);
+                    });
+                },
+                function(client, done, callback){
+                    // end transaction
+                    self.commit(client, done, callback);
+                }
+            ], function(err, result){
+                callback(err, result);
+            });
         }
     }
     return self;
