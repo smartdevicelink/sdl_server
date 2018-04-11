bd4882500765d4a5ea533365001ba6d81b73e42d
Nick Schwab <nick.schwab@livio.io>
2018-04-11 16:12:46 -0400

    + reserved app definitions (backed and UI)

--- migrations/sqls/20180410204352-reserved-application-definitions-down.sql
+++ migrations/sqls/20180410204352-reserved-application-definitions-down.sql
@@ -1,1 +1,12 @@
-/* Replace with your SQL commands */
+ALTER TABLE function_group_info
+DROP COLUMN IF EXISTS "is_pre_data_consent",
+DROP COLUMN IF EXISTS "is_device";
+
+CREATE OR REPLACE VIEW view_function_group_info AS
+SELECT function_group_info.*
+FROM (
+SELECT property_name, status, max(id) AS id
+    FROM function_group_info
+    GROUP BY property_name, status
+) AS vfgi
+INNER JOIN function_group_info ON vfgi.id = function_group_info.id;
--- migrations/sqls/20180410204352-reserved-application-definitions-up.sql
+++ migrations/sqls/20180410204352-reserved-application-definitions-up.sql
@@ -1,3 +1,12 @@
 ALTER TABLE function_group_info
 ADD COLUMN IF NOT EXISTS "is_pre_data_consent" BOOLEAN NOT NULL DEFAULT false,
 ADD COLUMN IF NOT EXISTS "is_device" BOOLEAN NOT NULL DEFAULT false;
+
+CREATE OR REPLACE VIEW view_function_group_info AS
+SELECT function_group_info.*
+FROM (
+SELECT property_name, status, max(id) AS id
+    FROM function_group_info
+    GROUP BY property_name, status
+) AS vfgi
+INNER JOIN function_group_info ON vfgi.id = function_group_info.id;
