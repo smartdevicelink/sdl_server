87d69e9d6a841e43bc41c486f6654fe689e77bb4
Christopher Rokita <chris@livio.io>
2018-04-18 14:00:32 -0400

    Fix broken mapped permissions view

--- migrations/sqls/20180410204352-reserved-application-definitions-up.sql
+++ migrations/sqls/20180410204352-reserved-application-definitions-up.sql
@@ -52,44 +52,28 @@ INNER JOIN module_config
 ON module_config.id = mc.id;
 
 CREATE OR REPLACE VIEW view_mapped_permissions AS
-SELECT function_group_id AS id, permission_name AS name, status, property_name
+SELECT function_group_id AS id, permission_name AS name, status, property_name, is_deleted
 FROM view_function_group_info
 INNER JOIN function_group_hmi_levels
 ON view_function_group_info.id = function_group_hmi_levels.function_group_id
+WHERE is_deleted=false
 UNION
-SELECT function_group_id AS id, parameter AS name, status, property_name
+SELECT function_group_id AS id, parameter AS name, status, property_name, is_deleted
 FROM view_function_group_info
 INNER JOIN function_group_parameters
-ON view_function_group_info.id = function_group_parameters.function_group_id;
+ON view_function_group_info.id = function_group_parameters.function_group_id
+WHERE is_deleted=false;
 
 CREATE OR REPLACE VIEW view_mapped_permissions_staging AS
-SELECT mapped.*
-FROM (
-    SELECT function_group_id AS id, permission_name AS name, status, property_name
-    FROM view_function_group_info
-    INNER JOIN function_group_hmi_levels
-    ON view_function_group_info.id = function_group_hmi_levels.function_group_id
-    UNION
-    SELECT function_group_id AS id, parameter AS name, status, property_name
-    FROM view_function_group_info
-    INNER JOIN function_group_parameters
-    ON view_function_group_info.id = function_group_parameters.function_group_id
-) mapped
+SELECT view_mapped_permissions.*
+FROM view_mapped_permissions
 INNER JOIN (
     SELECT max(id) AS id, property_name
     FROM view_function_group_info
     GROUP BY property_name
 ) fgi
-ON mapped.id = fgi.id;
+ON view_mapped_permissions.id = fgi.id;
 
 CREATE OR REPLACE VIEW view_mapped_permissions_production AS
-SELECT function_group_id AS id, permission_name AS name, status, property_name
-FROM view_function_group_info
-INNER JOIN function_group_hmi_levels
-ON view_function_group_info.id = function_group_hmi_levels.function_group_id
-UNION
-SELECT function_group_id AS id, parameter AS name, status, property_name
-FROM view_function_group_info
-INNER JOIN function_group_parameters
-ON view_function_group_info.id = function_group_parameters.function_group_id
+SELECT view_mapped_permissions.* FROM view_mapped_permissions
 WHERE status = 'PRODUCTION';
