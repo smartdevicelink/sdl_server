1704f2c40f40dae43cdd28b4ff33ecc4d9f3c4d8
ckachur <cameron@livio.io>
2018-04-24 11:14:54 -0400

    Add logic for displaying whether a funcitonal group is default or not.

--- src/components/common/CardItem.vue
+++ src/components/common/CardItem.vue
@@ -1,9 +1,15 @@
 <template>
     <router-link v-bind:to="link" v-bind:class="{ 'opacity-30': item.is_deleted }">
         <div>
-            <h5>{{ item.title }}<i v-if="environment != 'PRODUCTION'" class="pull-right fa fa-pencil display-hover" aria-hidden="true"></i></h5>
+            <h5>
+                {{ item.title }}
+                <i v-if="environment != 'PRODUCTION'" class="pull-right fa fa-pencil display-hover" aria-hidden="true"></i>
+            </h5>
             <div class="description">
                 {{ item.description }}
+            </div>
+            <div v-if="item.is_default_fg" class="color-green">
+                <p>DEFAULT</p>
             </div>
             <div class="permission-count">
                 {{ item.count || '0' }} {{ item.count == 1 ? count_label_singular : count_label_plural }}
