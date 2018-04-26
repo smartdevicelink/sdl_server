f898a42c30ab567d2a8654183984e85695599f02
ckachur <cameron@livio.io>
2018-04-26 12:37:36 -0400

    Fix layout of CardItem to incorporate default indicator.

--- src/components/common/CardItem.vue
+++ src/components/common/CardItem.vue
@@ -1,23 +1,33 @@
 <template>
     <router-link v-bind:to="link" v-bind:class="{ 'opacity-30': item.is_deleted }">
         <div>
-            <h5>
-                {{ item.title }}
-                <i v-if="environment != 'PRODUCTION'" class="pull-right fa fa-pencil display-hover" aria-hidden="true"></i>
-            </h5>
-            <div class="description">
-                {{ item.description }}
+            <div class="card-header">
+                <div class="card-title">
+                    <h5>
+                        {{ item.title }}
+                    </h5>
+                </div>
+                <div class="card-pencil">
+                    <i v-if="environment != 'PRODUCTION'" class="fa fa-pencil display-hover" style="float: right;" aria-hidden="true"></i>
+                </div>
             </div>
-            <div v-if="item.is_default" class="color-green">
-                <p>DEFAULT</p>
+            <div class="card-description">
+                <p>{{ item.description }}</p>
             </div>
-            <div class="permission-count">
-                {{ item.count || '0' }} {{ item.count == 1 ? count_label_singular : count_label_plural }}
+            <div class="card-default">
+                <div v-if="item.is_default" class="color-green">
+                    DEFAULT
+                </div>
             </div>
-            <div
-                v-if="item.status != 'PRODUCTION' || item.is_deleted"
-                class="func-status-tag color-green">
-                MODIFIED {{ item.is_deleted ? "(DELETED)" : "" }}
+            <div class="card-footer">
+                <div class="card-count">
+                    {{ item.count || '0' }} {{ item.count == 1 ? count_label_singular : count_label_plural }}
+                </div>
+                <div
+                    v-if="item.status != 'PRODUCTION' || item.is_deleted"
+                    class="card-status color-green">
+                    MODIFIED {{ item.is_deleted ? "(DELETED)" : "" }}
+                </div>
             </div>
         </div>
     </router-link>
