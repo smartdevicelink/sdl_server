23bf361ded09918d1051cc9fb5a566c59aea4009
Nick Schwab <nick.schwab@livio.io>
2018-07-30 16:57:24 -0400

    Cleanup

--- app/v1/policy/controller.js
+++ app/v1/policy/controller.js
@@ -14,12 +14,14 @@ function postFromCore (isProduction) {
 		if (res.errorMsg) {
 			return res.status(400).send({ error: res.errorMsg });
 		}
+		/*
 		console.log("module_meta");
 		console.log(JSON.stringify(req.body.policy_table.module_meta));
 		console.log("usage_and_error_counts");
 		console.log(JSON.stringify(req.body.policy_table.usage_and_error_counts));
 		console.log("device_data");
 		console.log(JSON.stringify(req.body.policy_table.device_data));
+		*/
         helper.generatePolicyTable(isProduction, req.body.policy_table.app_policies, true, handlePolicyTableFlow.bind(null, res, true));
 	}
 }
