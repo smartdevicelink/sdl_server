bbc7abda79d4d48e688b9c3d3fc90a156f00a88d
Russell Johnson <russell.johnson@livio.io>
2019-08-13 10:37:28 -0400

    property name should be Base-4

--- migrations/sqls/20190725145913-close-application-rpc-0115-down.sql
+++ migrations/sqls/20190725145913-close-application-rpc-0115-down.sql
@@ -2,12 +2,12 @@
 
 DELETE FROM function_group_hmi_levels WHERE permission_name='CloseApplication'
                                         AND hmi_level = 'BACKGROUND'
-                                        AND function_group_id IN (SELECT function_group_id FROM function_group_info WHERE property_name = 'AppServiceProviderGroup');
+                                        AND function_group_id IN (SELECT function_group_id FROM function_group_info WHERE property_name = 'Base-4');
 
 DELETE FROM function_group_hmi_levels WHERE permission_name='CloseApplication'
                                         AND hmi_level = 'FULL'
-                                        AND function_group_id IN (SELECT function_group_id FROM function_group_info WHERE property_name = 'AppServiceProviderGroup');
+                                        AND function_group_id IN (SELECT function_group_id FROM function_group_info WHERE property_name = 'Base-4');
 
 DELETE FROM function_group_hmi_levels WHERE permission_name='CloseApplication'
                                         AND hmi_level = 'LIMITED'
-                                        AND function_group_id IN (SELECT function_group_id FROM function_group_info WHERE property_name = 'AppServiceProviderGroup');
+                                        AND function_group_id IN (SELECT function_group_id FROM function_group_info WHERE property_name = 'Base-4');
