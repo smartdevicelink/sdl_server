0d54e02c6571eff1150ca3b1256e2c1e09910fa5
Russell Johnson <russell.johnson@livio.io>
2019-08-13 10:28:29 -0400

    update close application migration

--- migrations/sqls/20190725145913-close-application-rpc-0115-down.sql
+++ migrations/sqls/20190725145913-close-application-rpc-0115-down.sql
@@ -2,20 +2,12 @@
 
 DELETE FROM function_group_hmi_levels WHERE permission_name='CloseApplication'
                                         AND hmi_level = 'BACKGROUND'
-                                        AND function_group_id = (SELECT function_group_id FROM function_group_info WHERE property_name = 'AppServiceProviderGroup');
+                                        AND function_group_id IN (SELECT function_group_id FROM function_group_info WHERE property_name = 'AppServiceProviderGroup');
 
 DELETE FROM function_group_hmi_levels WHERE permission_name='CloseApplication'
                                         AND hmi_level = 'FULL'
-                                        AND function_group_id = (SELECT function_group_id FROM function_group_info WHERE property_name = 'AppServiceProviderGroup');
+                                        AND function_group_id IN (SELECT function_group_id FROM function_group_info WHERE property_name = 'AppServiceProviderGroup');
 
 DELETE FROM function_group_hmi_levels WHERE permission_name='CloseApplication'
                                         AND hmi_level = 'LIMITED'
-                                        AND function_group_id = (SELECT function_group_id FROM function_group_info WHERE property_name = 'AppServiceProviderGroup');
-
---no access to none given by default.
--- DELETE FROM function_group_hmi_levels WHERE permission_name='CloseApplication'
---                                         AND hmi_level = 'NONE'
---                                         AND function_group_id = (SELECT function_group_id FROM function_group_info WHERE property_name = 'AppServiceProviderGroup');
-
-
-DELETE FROM permissions where name = 'CloseApplication';
+                                        AND function_group_id IN (SELECT function_group_id FROM function_group_info WHERE property_name = 'AppServiceProviderGroup');
--- migrations/sqls/20190725145913-close-application-rpc-0115-up.sql
+++ migrations/sqls/20190725145913-close-application-rpc-0115-up.sql
@@ -20,19 +20,3 @@ SELECT id AS function_group_id, 'CloseApplication' AS permission_name, 'BACKGROU
 FROM function_group_info
 WHERE property_name = 'Base-4'
   AND NOT EXISTS(SELECT 1 FROM function_group_hmi_levels where permission_name='CloseApplication' and hmi_level='BACKGROUND');
-
---https://github.com/smartdevicelink/sdl_core/pull/2948/files
---no access to NONE by default it doesn't make sense.
--- INSERT INTO function_group_hmi_levels(function_group_id, permission_name, hmi_level)
--- SELECT id AS function_group_id, 'CloseApplication' AS permission_name, 'NONE' AS hmi_level
--- FROM function_group_info
--- WHERE property_name = 'Base-4'
---   AND NOT EXISTS(SELECT 1 FROM function_group_hmi_levels where permission_name='CloseApplication' and hmi_level='NONE')
--- ;
-
-
--- Check that the 6.0.0 version has the new rpc before approving to merge into develop.
--- https://github.com/smartdevicelink/rpc_spec/blob/version/6_0_0/MOBILE_API.xml#L2654
--- https://github.com/smartdevicelink/rpc_spec/pull/174/files
-INSERT INTO permissions(name,type,function_id,display_name)
-VALUES ('CloseApplication','RPC',58,'Close Application');
