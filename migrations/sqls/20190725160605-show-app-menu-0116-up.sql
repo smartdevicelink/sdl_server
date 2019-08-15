23995d7ca31fe9629165f949bde1d91562eed435
Russell Johnson <russell.johnson@livio.io>
2019-08-15 16:30:25 -0400

    check not exist before inserting ShowAppMenu

--- migrations/sqls/20190725160605-show-app-menu-0116-up.sql
+++ migrations/sqls/20190725160605-show-app-menu-0116-up.sql
@@ -7,9 +7,12 @@
 INSERT INTO function_group_hmi_levels(function_group_id, permission_name, hmi_level)
 SELECT id AS function_group_id, 'ShowAppMenu' AS permission_name, 'FULL' AS hmi_level
 FROM function_group_info
-WHERE property_name = 'Base-4';
+WHERE property_name = 'Base-4'
+  AND NOT EXISTS(SELECT 1 FROM function_group_hmi_levels where permission_name='ShowAppMenu' and hmi_level='FULL');
+
 
 INSERT INTO function_group_hmi_levels(function_group_id, permission_name, hmi_level)
 SELECT id AS function_group_id, 'ShowAppMenu' AS permission_name, 'FULL' AS hmi_level
 FROM function_group_info
-WHERE property_name = 'Base-6';
+WHERE property_name = 'Base-6'
+  AND NOT EXISTS(SELECT 1 FROM function_group_hmi_levels where permission_name='ShowAppMenu' and hmi_level='FULL');
