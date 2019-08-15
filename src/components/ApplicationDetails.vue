0056b168653ea3ae712e45198e2aa4e2b92b299a
Nick Schwab <nick.schwab@livio.io>
2019-08-15 15:20:33 -0400

    + close td tag

--- src/components/ApplicationDetails.vue
+++ src/components/ApplicationDetails.vue
@@ -51,6 +51,7 @@
                                     <td class="icon">
                                         <img v-if="app.icon_url" v-bind:src="app.icon_url" class="rounded" style="width: 40px; height: 40px;" />
                                         <img v-else class="rounded" style="width: 40px; height: 40px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20200%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15e9f9b8d79%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15e9f9b8d79%22%3E%3Crect%20width%3D%22200%22%20height%3D%22200%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2274.4296875%22%20y%3D%22104.5%22%3E200x200%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" />
+                                    </td>
                                     <td class="title">{{ app.name }}</td>
                                     <td>{{ app.updated_ts }}</td>
                                     <td>{{ app.platform }}</td>
