cbd685905d45b31461120a658a945cafdedd67d5
Russell Johnson <russell.johnson@livio.io>
2019-08-20 11:38:28 -0400

    combined insert statement and added not exists condition.

--- migrations/sqls/20190725182023-cancel-interaction-rpc-0184-up.sql
+++ migrations/sqls/20190725182023-cancel-interaction-rpc-0184-up.sql
@@ -1,23 +1,18 @@
 /*  db-migrate create cancel-interaction-rpc-0184 -e pg-staging */
 
---https://github.com/smartdevicelink/sdl_evolution/blob/master/proposals/0184-cancel-interaction.md
-
-
---The defaults are based on https://github.com/smartdevicelink/sdl_core/blob/develop/src/appMain/sdl_preloaded_pt.json
---https://github.com/smartdevicelink/sdl_core/pull/2963/files
-
 INSERT INTO function_group_hmi_levels(function_group_id, permission_name, hmi_level)
-SELECT id AS function_group_id, 'CancelInteraction' AS permission_name, 'FULL' AS hmi_level
+(SELECT id AS function_group_id, 'CancelInteraction' AS permission_name, 'FULL'::hmi_level AS hmi_level
 FROM function_group_info
-WHERE property_name = 'Base-4';
-
-
-INSERT INTO function_group_hmi_levels(function_group_id, permission_name, hmi_level)
-SELECT id AS function_group_id, 'CancelInteraction' AS permission_name, 'LIMITED' AS hmi_level
+WHERE property_name = 'Base-4'
+AND NOT EXISTS(SELECT 1 from function_group_hmi_levels where property_name = 'Base-4' AND permission_name = 'CancelInteraction'))
+UNION
+(SELECT id AS function_group_id, 'CancelInteraction' AS permission_name, 'LIMITED'::hmi_level AS hmi_level
 FROM function_group_info
-WHERE property_name = 'Base-4';
-
-INSERT INTO function_group_hmi_levels(function_group_id, permission_name, hmi_level)
-SELECT id AS function_group_id, 'CancelInteraction' AS permission_name, 'BACKGROUND' AS hmi_level
+WHERE property_name = 'Base-4'
+AND NOT EXISTS(SELECT 1 from function_group_hmi_levels where property_name = 'Base-4' AND permission_name = 'CancelInteraction'))
+UNION
+(SELECT id AS function_group_id, 'CancelInteraction' AS permission_name, 'BACKGROUND'::hmi_level AS hmi_level
 FROM function_group_info
-WHERE property_name = 'Base-4';
+WHERE property_name = 'Base-4'
+AND NOT EXISTS(SELECT 1 from function_group_hmi_levels where property_name = 'Base-4' AND permission_name = 'CancelInteraction'))
+;
