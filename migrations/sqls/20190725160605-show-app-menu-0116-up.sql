9c6b8a4ec77c1f67cd95bc67eaf9c204ddfe5e0b
Russell Johnson <russell.johnson@livio.io>
2019-08-20 11:18:01 -0400

    use single insert statement for the function_group_hmi_levels. use hmi_level enumeration instead of string for insert.

--- migrations/sqls/20190725160605-show-app-menu-0116-up.sql
+++ migrations/sqls/20190725160605-show-app-menu-0116-up.sql
@@ -1,18 +1,10 @@
 /* db-migrate create show-app-menu-0116 -e pg-staging */
 
 
---The defaults are based on https://github.com/smartdevicelink/sdl_core/blob/develop/src/appMain/sdl_preloaded_pt.json
---https://github.com/smartdevicelink/sdl_core/blob/feature/open_menu_rpc/src/appMain/sdl_preloaded_pt.json#L51
---Only allows full by default here. It is a menu related command so that makes sense.
 INSERT INTO function_group_hmi_levels(function_group_id, permission_name, hmi_level)
-SELECT id AS function_group_id, 'ShowAppMenu' AS permission_name, 'FULL' AS hmi_level
-FROM function_group_info
-WHERE property_name = 'Base-4'
-  AND NOT EXISTS(SELECT 1 FROM function_group_hmi_levels where permission_name='ShowAppMenu' and hmi_level='FULL');
-
-
-INSERT INTO function_group_hmi_levels(function_group_id, permission_name, hmi_level)
-SELECT id AS function_group_id, 'ShowAppMenu' AS permission_name, 'FULL' AS hmi_level
-FROM function_group_info
-WHERE property_name = 'Base-6'
-  AND NOT EXISTS(SELECT 1 FROM function_group_hmi_levels where permission_name='ShowAppMenu' and hmi_level='FULL');
+(SELECT id AS function_group_id, 'ShowAppMenu' AS permission_name, 'FULL'::hmi_level AS hmi_level FROM function_group_info
+WHERE property_name = 'Base-4' AND NOT EXISTS(SELECT 1 FROM function_group_hmi_levels WHERE permission_name='ShowAppMenu' AND hmi_level='FULL'::hmi_level))
+UNION
+(SELECT id AS function_group_id, 'ShowAppMenu' AS permission_name, 'FULL'::hmi_level AS hmi_level FROM function_group_info
+WHERE property_name = 'Base-6' AND NOT EXISTS(SELECT 1 FROM function_group_hmi_levels WHERE permission_name='ShowAppMenu' AND hmi_level='FULL'::hmi_level))
+;
