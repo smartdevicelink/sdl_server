eefd41a1e87f1652889bd716670d5226eeac9a3e
Russell Johnson <russell.johnson@livio.io>
2019-08-22 12:13:04 -0400

    use cross join for the single insert into statement

--- migrations/sqls/20190725182023-cancel-interaction-rpc-0184-down.sql
+++ migrations/sqls/20190725182023-cancel-interaction-rpc-0184-down.sql
@@ -1,13 +1,1 @@
-
-
-DELETE FROM function_group_hmi_levels WHERE permission_name='CancelInteraction'
-                                        AND hmi_level = 'FULL'
-                                        AND function_group_id IN (SELECT function_group_id FROM function_group_info WHERE property_name = 'Base-4' GROUP BY function_group_id);
-
-DELETE FROM function_group_hmi_levels WHERE permission_name='CancelInteraction'
-                                        AND hmi_level = 'LIMITED'
-                                        AND function_group_id IN (SELECT function_group_id FROM function_group_info WHERE property_name = 'Base-4' GROUP BY function_group_id);
-
-DELETE FROM function_group_hmi_levels WHERE permission_name='CancelInteraction'
-                                        AND hmi_level = 'BACKGROUND'
-                                        AND function_group_id IN (SELECT function_group_id FROM function_group_info WHERE property_name = 'Base-4' GROUP BY function_group_id);
+/** No down migration required for function_group_hmi_levels */
