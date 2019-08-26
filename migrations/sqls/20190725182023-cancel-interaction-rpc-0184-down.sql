373abf6e0e5af04e650c6b3ec70978381e5d0a1a
Russell Johnson <russell.johnson@livio.io>
2019-08-26 13:23:56 -0400

    updated indentation

--- migrations/sqls/20190725182023-cancel-interaction-rpc-0184-down.sql
+++ migrations/sqls/20190725182023-cancel-interaction-rpc-0184-down.sql
@@ -1,1 +1,1 @@
-/** No down migration required for function_group_hmi_levels */
+/** Intentionally left blank. If an OEM runs a down migration, we do not want to delete functional groups that may have been updated. **/
--- migrations/sqls/20190725182023-cancel-interaction-rpc-0184-up.sql
+++ migrations/sqls/20190725182023-cancel-interaction-rpc-0184-up.sql
@@ -3,14 +3,13 @@
 INSERT INTO function_group_hmi_levels(function_group_id, permission_name, hmi_level)
 SELECT id AS function_group_id, 'CancelInteraction' AS permission_name, v.hmi_level::hmi_level AS hmi_level
 FROM function_group_info
-         CROSS JOIN (
+CROSS JOIN (
     VALUES ('FULL'), ('LIMITED'), ('BACKGROUND')
 ) AS v(hmi_level)
 WHERE property_name = 'Base-4'
-  AND NOT EXISTS(
-        SELECT 1
-        FROM function_group_hmi_levels
+    AND NOT EXISTS(
+    SELECT 1
+    FROM function_group_hmi_levels
         WHERE property_name = 'Base-4'
-          AND permission_name = 'CancelInteraction'
+            AND permission_name = 'CancelInteraction'
     );
-;
