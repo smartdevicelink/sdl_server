1a25c95ed951a89e69cb976e0d853105910d83f6
Russell Johnson <russell.johnson@livio.io>
2019-08-27 10:08:30 -0400

    updated spacing added comment as to why there is no down migration.

--- migrations/sqls/20190715202037-update-published-app-services-up.sql
+++ migrations/sqls/20190715202037-update-published-app-services-up.sql
@@ -3,13 +3,13 @@
 INSERT INTO function_group_hmi_levels(function_group_id, permission_name, hmi_level)
 SELECT id AS function_group_id, 'UnpublishAppService' AS permission_name, v.hmi_level::hmi_level AS hmi_level
 FROM function_group_info
-         CROSS JOIN (
+CROSS JOIN (
     VALUES ('FULL'), ('LIMITED'), ('BACKGROUND'), ('NONE')
 ) AS v(hmi_level)
 WHERE property_name = 'AppServiceProviderGroup'
-  AND NOT EXISTS(
+    AND NOT EXISTS(
         SELECT 1
         FROM function_group_hmi_levels
         WHERE property_name = 'AppServiceProviderGroup'
-          AND permission_name = 'UnpublishAppService'
+            AND permission_name = 'UnpublishAppService'
     );
