a30d767700bf51b7a3df50e2ef54a3b7657a3ba2
Russell Johnson <russell.johnson@livio.io>
2019-08-29 14:16:34 -0400

    update spacing

--- migrations/sqls/20190823181120-widget-support-0216-down.sql
+++ migrations/sqls/20190823181120-widget-support-0216-down.sql
@@ -1,1 +1,1 @@
-/* Replace with your SQL commands */
+/** Intentionally left blank. If an OEM runs a down migration, we do not want to delete functional groups that may have been updated. **/
--- migrations/sqls/20190823181120-widget-support-0216-up.sql
+++ migrations/sqls/20190823181120-widget-support-0216-up.sql
@@ -3,8 +3,9 @@
 
 INSERT INTO function_group_info (property_name, user_consent_prompt, is_default, status)
 SELECT 'WidgetSupport' AS property_name, null AS user_consent_prompt, false AS is_default, 'PRODUCTION' AS status
-WHERE NOT EXISTS (
-        SELECT * FROM function_group_info fgi
+WHERE NOT EXISTS(
+        SELECT *
+        FROM function_group_info fgi
         WHERE fgi.property_name = 'WidgetSupport'
     );
 
@@ -13,57 +14,51 @@ WHERE NOT EXISTS (
 INSERT INTO function_group_hmi_levels(function_group_id, permission_name, hmi_level)
 SELECT id AS function_group_id, 'CreateWindow' AS permission_name, v.hmi_level::hmi_level AS hmi_level
 FROM function_group_info
-         CROSS JOIN (
+CROSS JOIN (
     VALUES ('FULL'), ('LIMITED'), ('BACKGROUND')
 ) AS v(hmi_level)
 WHERE property_name = 'WidgetSupport'
-  AND NOT EXISTS(
+    AND NOT EXISTS(
         SELECT 1
         FROM function_group_hmi_levels
-        WHERE property_name = 'WidgetSupport'
-          AND permission_name = 'CreateWindow'
+        WHERE property_name = 'WidgetSupport' AND permission_name = 'CreateWindow'
     );
-
 
 INSERT INTO function_group_hmi_levels(function_group_id, permission_name, hmi_level)
 SELECT id AS function_group_id, 'DeleteWindow' AS permission_name, v.hmi_level::hmi_level AS hmi_level
 FROM function_group_info
-         CROSS JOIN (
+CROSS JOIN (
     VALUES ('FULL'), ('LIMITED'), ('BACKGROUND')
 ) AS v(hmi_level)
 WHERE property_name = 'WidgetSupport'
-  AND NOT EXISTS(
+    AND NOT EXISTS(
         SELECT 1
         FROM function_group_hmi_levels
-        WHERE property_name = 'WidgetSupport'
-          AND permission_name = 'DeleteWindow'
+        WHERE property_name = 'WidgetSupport' AND permission_name = 'DeleteWindow'
     );
 
 INSERT INTO function_group_hmi_levels(function_group_id, permission_name, hmi_level)
 SELECT id AS function_group_id, 'Show' AS permission_name, v.hmi_level::hmi_level AS hmi_level
 FROM function_group_info
-         CROSS JOIN (
+CROSS JOIN (
     VALUES ('FULL'), ('LIMITED'), ('BACKGROUND')
 ) AS v(hmi_level)
 WHERE property_name = 'WidgetSupport'
-  AND NOT EXISTS(
+    AND NOT EXISTS(
         SELECT 1
         FROM function_group_hmi_levels
-        WHERE property_name = 'WidgetSupport'
-          AND permission_name = 'Show'
+        WHERE property_name = 'WidgetSupport' AND permission_name = 'Show'
     );
-
 
 INSERT INTO function_group_hmi_levels(function_group_id, permission_name, hmi_level)
 SELECT id AS function_group_id, 'OnSystemCapabilityUpdated' AS permission_name, v.hmi_level::hmi_level AS hmi_level
 FROM function_group_info
-         CROSS JOIN (
+CROSS JOIN (
     VALUES ('FULL'), ('LIMITED'), ('BACKGROUND'), ('NONE')
 ) AS v(hmi_level)
 WHERE property_name = 'WidgetSupport'
-  AND NOT EXISTS(
+    AND NOT EXISTS(
         SELECT 1
         FROM function_group_hmi_levels
-        WHERE property_name = 'WidgetSupport'
-          AND permission_name = 'OnSystemCapabilityUpdated'
+        WHERE property_name = 'WidgetSupport' AND permission_name = 'OnSystemCapabilityUpdated'
     );
